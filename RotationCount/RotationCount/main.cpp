5f93f44a49138182aa7be7b0f8f48d45e529ea39
Sunny-Vats <ssunnyvvats.0608@gmail.com>
2021-03-20 01:07:25 +0530

    Final Program to find no of rotation done to sorted array

--- RotationCount/RotationCount/main.cpp
+++ RotationCount/RotationCount/main.cpp
@@ -8,23 +8,24 @@
 #include <iostream>
 using namespace std;
 
-int findNoOfRotation(int arr[], int left, int right) {
-    int count=0;
-    if(arr[right] > arr[left] )
-        return left;
-    if(arr[right] == arr[left])
-        return left;
-    int mid = (left + right) / 2;
-    if(arr[right] < arr[left] && arr[right+1] > arr[mid]) {
-        return right;
-    }
-    else
-        findNoOfRotation(arr, 0, mid-1);
-    return count;
+int findNoOfRotation(int arr[], int leftIndex, int rightIndex) {
+    // Condition to check whether there is single element in array
+    if( leftIndex == rightIndex ) return leftIndex;
+    // Condition to check if array is rotated at all
+    if( arr[leftIndex] < arr[rightIndex] ) return leftIndex;
+    int mid = ( leftIndex + rightIndex ) / 2;
+    // Condition if rotation point found at mid
+    if( arr[mid] > arr[leftIndex] && arr[mid+1] < arr[mid] ) return (mid+1);
+    // Condition if rotation point found at mid+1
+    if( arr[mid] < arr[leftIndex] && arr[mid] < arr[mid-1] ) return mid;
+    // Condition if element at mid is greater than element at leftIndex
+    if( arr[rightIndex] > arr[mid] )
+        return findNoOfRotation(arr, leftIndex, mid-1);
+    return findNoOfRotation(arr, mid+1, rightIndex);
 }
 
 int main() {
-    int arr[] = {15, 18, 2, 8, 10};
+    int arr[] = {3, 4, 5, 5, 1};
     int size = sizeof(arr)/sizeof(arr[0]);
     cout << findNoOfRotation(arr, 0, size-1) << endl;
     return 0;
