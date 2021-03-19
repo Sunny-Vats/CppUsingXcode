0a0c76e88daf3d12facf177d3792291797e82373
Sunny Vats <69765771+Sunny-Vats@users.noreply.github.com>
2021-03-20 01:16:20 +0530

    Update main.cpp
    Line21: mid is lesser instead of mid is greater
    Line21: rightIndex istead of leftIndex
    Although either can be used to compare mid in Line22

--- RotationCount/RotationCount/main.cpp
+++ RotationCount/RotationCount/main.cpp
@@ -18,7 +18,7 @@ int findNoOfRotation(int arr[], int leftIndex, int rightIndex) {
     if( arr[mid] > arr[leftIndex] && arr[mid+1] < arr[mid] ) return (mid+1);
     // Condition if rotation point found at mid+1
     if( arr[mid] < arr[leftIndex] && arr[mid] < arr[mid-1] ) return mid;
-    // Condition if element at mid is greater than element at leftIndex
+    // Condition if element at mid is lesser than element at rightIndex
     if( arr[rightIndex] > arr[mid] )
         return findNoOfRotation(arr, leftIndex, mid-1);
     return findNoOfRotation(arr, mid+1, rightIndex);
