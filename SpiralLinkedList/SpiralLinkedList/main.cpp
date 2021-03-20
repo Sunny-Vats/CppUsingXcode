1de403762a374b7deafe2bec1f4f584bde1a52b3
Sunny-Vats <ssunnyvvats.0608@gmail.com>
2021-03-20 17:38:55 +0530

    Spiral Linked List implementation

---
+++ SpiralLinkedList/SpiralLinkedList/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SpiralLinkedList
+//
+//  Created by Sunny Vats on 20/03/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
