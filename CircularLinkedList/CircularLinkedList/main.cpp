5a61958c02539c03f0800552987b4723202260ef
Sunny-Vats <ssunnyvvats.0608@gmail.com>
2021-03-24 14:12:12 +0530

    Circular Linked List Implementation (partial complete)

---
+++ CircularLinkedList/CircularLinkedList/main.cpp
@@ -0,0 +1,93 @@
+//
+//  main.cpp
+//  CircularLinkedList
+//
+//  Created by Sunny Vats on 23/03/21.
+//
+
+#include <iostream>
+using namespace std;
+class CircularLinked {
+    struct node {
+        int data;
+        node *next;
+    } *LAST;
+public:
+    CircularLinked();
+    void addNode(int element);
+    bool search(int element, node **previous, node **current);   // Implemented completely
+    bool delNode(int element);                                   // Not completed.. Need help
+    void display();
+};
+
+CircularLinked::CircularLinked() {
+    LAST = NULL;
+}
+
+void CircularLinked::addNode(int element) {
+    if(LAST == NULL) {
+        LAST = new node;
+        LAST -> data = element;
+        LAST -> next = LAST;
+    }
+    else {
+        node *current = LAST, *temp = new node;
+        temp -> data = element;
+        temp -> next = current -> next;
+        LAST -> next = temp;
+        LAST = temp;
+    }
+}
+
+bool CircularLinked::search(int element, node **pre, node **current) {
+    while( (*current) -> data != element && (*current) -> next != LAST ) {
+        *pre = *current;
+        *current = (*current) -> next;
+    }
+    return ((*current) -> data == element);
+}
+
+bool CircularLinked::delNode(int element) {
+    node *temp = LAST;
+    node **current = &(temp -> next), **pre = &temp;
+    search(element, pre, current);
+    return ((*current) -> data == element);
+}
+
+void CircularLinked::display() {
+    node *current = LAST -> next;
+    node *temp = current;
+    cout << ".-->";
+    do {
+        cout << temp -> data << " ---> ";
+        temp = temp -> next;
+    } while(temp != LAST);
+    cout << temp -> data << " <--." << endl;
+    temp = LAST -> next;
+    cout << " \\___";
+    while(temp != LAST) {
+        cout << "________";
+        temp = temp -> next;
+    }
+    cout << "____/" << endl;
+}
+
+int main() {
+    CircularLinked obj;
+    for(int i = 12; i < 21; i++)
+    obj.addNode(i);
+    obj.display();
+    cout << endl;
+    int element = 20;
+    bool result = obj.delNode(element);     // Not completed yet
+    cout << "Delete element " << element << " result: " << result << endl;
+    /**
+     * .--> 12  --->  13  --->  14  --->  15  --->  16  --->  17  --->  18  --->  19 - -->  20  <--.
+     * \______________________________________________________________/
+     * ^ Output ^
+     * Delete element 20 result: 1
+     * Program ended with exit code: 0
+     */
+    obj.display();                          // Error in display
+    return 0;
+}
