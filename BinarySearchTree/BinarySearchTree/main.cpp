cbfb4783055c3180a8bb16ab806fed6c589f383d
Sunny-Vats <ssunnyvvats.0608@gmail.com>
2021-03-31 02:03:53 +0530

    Add implementation

--- BinarySearchTree/BinarySearchTree/main.cpp
+++ BinarySearchTree/BinarySearchTree/main.cpp
@@ -14,13 +14,66 @@ class BinarySearchTree {
         node *right;
     }*ROOT;
 public:
-    void addNode();
+    BinarySearchTree();
+    void addNode(int data);
     void traverse();
-    void addNodeBeg();
-    void addNodeEnd();
+    void search(int data);
+    void inOrder(node *ptr);
 };
+BinarySearchTree::BinarySearchTree() {
+    ROOT = NULL;
+}
+void BinarySearchTree::addNode( int data ) {
+    if( ROOT != NULL ) {
+        node *current = new node;
+        current -> data = data;
+        current -> left = NULL;
+        current -> right = NULL;
+        node *tmp = ROOT;
+        node *parent = NULL;
+        while( tmp != NULL ) {
+            parent = tmp;
+            if( parent -> data > current -> data )
+                tmp = tmp -> left;
+            else
+                tmp = tmp -> right;
+        }
+        if( parent -> data > current -> data )
+            parent -> left = current;
+        else
+            parent -> right = current;
+    }
+    else {
+        ROOT = new node;
+        ROOT -> data = data;
+        ROOT -> left = NULL;
+        ROOT -> right = NULL;
+    }
+    cout << data << " inserted." << endl;
+}
+void BinarySearchTree::traverse() {
+    if( ROOT == NULL ) cout << "Tree is empty.. No  on this tree." << endl;
+    else {
+        node *tmp = ROOT;
+        cout << endl << "INORDER TRAVERSAL" << endl;
+        inOrder(tmp);
+        cout << "NULL" << endl;
+    }
+}
+void BinarySearchTree::inOrder(node *ptr) {
+    if( ptr != NULL ) {
+        inOrder(ptr -> left);
+        cout << ptr -> data << " --> ";
+        inOrder(ptr -> right);
+    }
+}
 
 int main() {
-    
+    BinarySearchTree obj;
+    obj.addNode(34);
+    obj.addNode(22);
+    obj.addNode(45);
+    obj.addNode(18);
+    obj.traverse();
     return 0;
 }
