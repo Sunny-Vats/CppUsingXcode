156389c674a39947f5f23d5ea6ecdafae49281e0
Sunny-Vats <ssunnyvvats.0608@gmail.com>
2021-04-02 12:09:50 +0530

    Insertion Sort

---
+++ InsertionSort/InsertionSort/main.cpp
@@ -0,0 +1,44 @@
+//
+//  main.cpp
+//  InsertionSort
+//
+//  Created by Sunny Vats on 02/04/21.
+//
+
+#include <iostream>
+using namespace std;
+
+void insertionSort(int *arr, int len);
+void display(int *arr, int len);
+
+int main() {
+    int arr[] {5,4,1,3,2};
+    int len = sizeof(arr)/sizeof(int);
+    cout << "Array to be sorted: ";
+    display(arr, len);
+    insertionSort(arr, len);
+    cout << "Sorted array: " << endl;
+    display(arr, len);
+    return 0;
+}
+
+void insertionSort(int *arr, int len) {
+    for (int j = 1; j < len; j++) {
+        int key = arr[j];
+        int i = j - 1;
+        while( i >= 0 && key < arr[i] ) {
+            arr[i+1] = arr[i];
+            i--;
+        }
+        arr[i+1] = key;
+    }
+}
+
+void display(int *arr, int len) {
+    int i = 0;
+    while( len > i) {
+        cout << *(arr+i) << " ";
+        i++;
+    }
+    cout << endl << "±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±" << endl;
+}
